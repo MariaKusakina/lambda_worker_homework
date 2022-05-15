de1606866d1fa9100ac1cca72d02202c9e195e20
MariaKusakina <m.a.kusakina@gmail.com>
2022-05-15 19:08:41 +0300

    задача решена

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        OnTaskDoneListener listener = System.out::println;
+        OnTaskErrorListener errorCallback = System.out::println;
+        Worker worker = new Worker(listener, errorCallback);
+        worker.start();
     }
 }
